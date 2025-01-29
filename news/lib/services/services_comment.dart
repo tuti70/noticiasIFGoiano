ab9c15e8bad3566298fd038df3f87335870d14df
otuti Code <arthur.renato.nv@gmail.com>
2025-01-29 14:36:37 -0300

    ✨ feat: shared_preferences

--- news/lib/services/services_comment.dart
+++ news/lib/services/services_comment.dart
@@ -6,11 +6,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ServicesComment {
   Future<List<Comment>> getAll() async {
-    //Buscar ultimo ID de noticia salvo
-    int? commentId;
-    int? vewId;
+    //Recupera o último ID já recuperado na API
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int? lastNewsId = await prefs.getInt('lastNewsId');
 
-    String url = "http://157.245.254.121/noticias/?lastId=${commentId}";
+    String url = "http://157.245.254.121/noticias/?lastId=$lastNewsId&limit=20";
     Map<String, String> Headers = {
       HttpHeaders.contentTypeHeader: "application/json'; charset=UTF-8;",
     };
@@ -18,13 +18,6 @@ class ServicesComment {
     final response = await http.get(Uri.parse(url), headers: Headers);
 
     List<Comment> comments = List.empty(growable: true);
-
-    void VewId() async {
-      final prefs = await SharedPreferences.getInstance();
-      prefs.setInt('vewId', 4289);
-    }
-
-    vewId = commentId;
 
     if (response.statusCode == 200) {
       //Lista onde cada linha é um Map<String, dynamic>
@@ -35,48 +28,48 @@ class ServicesComment {
         comments.add(Comment.fromJson(mComment));
       });
 
-      //Recupera a ultima noticia da lista
       //Atualiza o sharedPrefernece com o ultimo ID de noticia recuperado
-      commentId = comments.last.id;
+      lastNewsId = comments.last.idSite;
+      prefs.setInt('lastNewsId', lastNewsId);
     } else {
       Exception('Failed to load comments');
     }
     return comments;
   }
 
-  Future<List<Comment>> getIfs() async {
-    //Buscar ultimo ID de noticia salvo
-    int? commentId;
+  // Future<List<Comment>> getIfs() async {
+  //   //Buscar ultimo ID de noticia salvo
+  //   int? commentId;
 
-    String url =
-        "http://157.245.254.121/noticias/?lastId=${commentId}&campus=Morrinhos'";
-    Map<String, String> Headers = {
-      HttpHeaders.contentTypeHeader: "application/json'; charset=UTF-8;",
-    };
+  //   String url =
+  //       "http://157.245.254.121/noticias/?lastId=${commentId}&campus=Morrinhos'";
+  //   Map<String, String> Headers = {
+  //     HttpHeaders.contentTypeHeader: "application/json'; charset=UTF-8;",
+  //   };
 
-    final response = await http.get(Uri.parse(url), headers: Headers);
+  //   final response = await http.get(Uri.parse(url), headers: Headers);
 
-    List<Comment> comments = List.empty(growable: true);
+  //   List<Comment> comments = List.empty(growable: true);
 
-    void VewId() async {
-      final prefs = await SharedPreferences.getInstance();
-      prefs.setInt('vewId', 4289);
-    }
+  //   void VewId() async {
+  //     final prefs = await SharedPreferences.getInstance();
+  //     prefs.setInt('vewId', 4289);
+  //   }
 
-    if (response.statusCode == 200) {
-      //Lista onde cada linha é um Map<String, dynamic>
-      List mComments = jsonDecode(response.body);
+  //   if (response.statusCode == 200) {
+  //     //Lista onde cada linha é um Map<String, dynamic>
+  //     List mComments = jsonDecode(response.body);
 
-      //Para cada linha, converte a linha Map<String, dynamic> em Objeto Post
-      mComments.forEach((mComment) {
-        comments.add(Comment.fromJson(mComment));
-      });
+  //     //Para cada linha, converte a linha Map<String, dynamic> em Objeto Post
+  //     mComments.forEach((mComment) {
+  //       comments.add(Comment.fromJson(mComment));
+  //     });
 
-      //Recupera a ultima noticia da lista
-      //Atualiza o sharedPrefernece com o ultimo ID de noticia recuperado
-    } else {
-      Exception('Failed to load comments');
-    }
-    return comments;
-  }
+  //     //Recupera a ultima noticia da lista
+  //     //Atualiza o sharedPrefernece com o ultimo ID de noticia recuperado
+  //   } else {
+  //     Exception('Failed to load comments');
+  //   }
+  //   return comments;
+  // }
 }
